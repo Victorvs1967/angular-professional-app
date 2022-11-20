e6786e43447f6be31b20c1554c37ff6f547a6935
Victorvs1967 <victorsmirnov@me.com>
2022-11-20 16:54:55 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'angular-professional-app';
+}
