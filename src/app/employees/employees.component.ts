4a35237e87130c1b809aac7452cfdb8c70474cdb
Victorvs1967 <victorsmirnov@me.com>
2022-11-21 20:20:53 +0200

    add filtered

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from '../models/employee.model';
 import employees from './data/employees.json';
 
 @Component({
@@ -8,9 +9,26 @@ import employees from './data/employees.json';
 })
 export class EmployeesComponent {
 
-  employees: any[] = employees;
+  employees: Employee[] = employees;
   title: string = 'Employee Management Solutions - EMS';
   showIcon: boolean = false;
+
+  private _designationFilter: string = '';
+  message: string = '';
+  filteredEmployees: Employee[] = employees;
+
+  set designationFilter(value: string) {
+    this._designationFilter = value;
+    this.filterByDestignation();
+  }
+
+  get designationFilter(): string {
+    return this._designationFilter;
+  }
+
+  filterByDestignation() {
+    this.filteredEmployees = this.employees.filter(employee => employee.designation.includes(this._designationFilter));
+  }
 
   toggleIcon() {
     this.showIcon = !this.showIcon;
---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,8 @@
+export interface Employee {
+  icon: string;
+  firstName: string;
+  lastName: string;
+  designation: string; 
+  insuratioceBalances: number; 
+  rbgProgress: number;
+}
