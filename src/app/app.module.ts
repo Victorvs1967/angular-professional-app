efac40f53aac1683aad91e2bc9b37f9bd0a2f8af
Victorvs1967 <victorsmirnov@me.com>
2022-11-21 21:52:56 +0200

    edited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
-import { ProgressBarComponent } from './employees/shared/progress-bar/progress-bar.component';
+import { ProgressBarComponent } from './shared/progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [
